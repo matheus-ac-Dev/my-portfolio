d56b683c5c0cbf0c0adb7a63a1f0931c3b88471c
matheus-ac-Dev <matheusalmeidacostaaaa@gmail.com>
2025-08-03 19:30:37 -0300

    feat(home): adicionar apresentação inicial na página principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
 export default function Home() {
   return (
-    <section>
-      <h1 className="text-3xl font-bold mb-4">Bem-vindo ao meu portfólio</h1>
-      <p>Projeto criado com Next.js e Tailwind CSS.</p>
+    <section className="space-y-6">
+      <h1 className="text-4xl font-bold">Olá, eu sou Matheus</h1>
+        <p className="text-lg text-zinc-700 dark:text-zinc-300">
+          Atualmente estudando Desenvolvimento de Sistemas e explorando diferentes áreas da tecnologia.  
+          Interesso-me por web, automações com IA e uso de ferramentas para ganhar produtividade.  
+          Estou em constante aprendizado, sempre buscando evoluir com projetos práticos.
+        </p>
+
+        <p className="text-zinc-600 dark:text-zinc-400">
+          Explore meus projetos, leia mais sobre mim ou entre em contato.
+        </p>
     </section>
   )
 }
